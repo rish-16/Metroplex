8f1c4ac0abed4a223d1b2692835233f3dd61d1f8
rish-16 <mail.rishabh.anand@gmail.com>
2020-06-29 15:54:07 +0800

    Add iterative shape superimposition

--- src/main.py
+++ src/main.py
@@ -1,39 +1,77 @@
+import cv2, math
 import numpy as np
 import matplotlib.pyplot as plt
+from collections import defaultdict
 from PIL import Image
 from shape import Shape
 
 def read_image(path):
     img = Image.open(path)
-    img = np.array(img)
     
     constraints = {}
-    constraints['width'] = max(img.shape[:2])
-    constraints['height'] = min(img.shape[:2])
+    constraints['width'] = max(np.array(img).shape[:2])
+    constraints['height'] = min(np.array(img).shape[:2])
     
     return img, constraints
     
-def get_loss(og_img, generated):
-    diff = np.square(og_img - generated)
+# standard MSE loss
+def get_loss(img1, img2):
+    diff = np.square(img1 - img2)
     cost = np.sum(diff)
-    cost = float(cost / (og_img.shape[0] * og_img.shape[1]))
+    cost = float(cost / (img1.shape[0] * img1.shape[1] * img1.shape[2]))
     
     return cost
     
+def draw_angled_rec(x0, y0, x1, y1, colour, angle, img):
+    _angle = angle * math.pi / 180.0
+    width = x1 - x0
+    height = y1 - y0
+    
+    b = math.cos(_angle) * 0.5
+    a = math.sin(_angle) * 0.5
+    pt0 = (int(x0 - a * height - b * width),
+           int(y0 + b * height - a * width))
+    pt1 = (int(x0 + a * height - b * width),
+           int(y0 - b * height - a * width))
+    pt2 = (int(2 * x0 - pt0[0]), int(2 * y0 - pt0[1]))
+    pt3 = (int(2 * x0 - pt1[0]), int(2 * y0 - pt1[1]))
+
+    cv2.line(img, pt0, pt1, colour, -1)
+    cv2.line(img, pt1, pt2, colour, -1)
+    cv2.line(img, pt2, pt3, colour, -1)
+    cv2.line(img, pt3, pt0, colour, -1)
+    
+    return img
+    
 img, constraints = read_image("../assets/sky.jpg")
-constraints['all_colours'] = list(set(tuple(v) for m2d in img for v in m2d))
+by_color = defaultdict(int)
+for pixel in img.getdata():
+    by_color[pixel] += 1
+constraints['all_colours'] = list(by_color.keys())
+
+img = np.array(img)
 average_colour = img.mean(axis=0).mean(axis=0)
-canvas = np.full(shape=img.shape, fill_value=average_colour, dtype=np.int)
-        
-generations = 1000
+
 n_shapes = 100
 
-shapes = [Shape(constraints) for i in range(n_shapes)]
+losses = []
 
-for gen in range(generations):
-    for i in range(len(shapes)):
-        cur_shape = shapes[i]
-        
-        img, gen_img = cur_shape.superimpose(img)
-        
-        
+canvas = np.full(shape=img.shape, fill_value=average_colour, dtype=np.int8)
+for i in range(n_shapes):
+    cur_shape = Shape(constraints)
+    
+    canvas = cur_shape.superimpose(canvas)
+    
+    loss = get_loss(img, canvas)
+    print (loss)
+    losses.append(loss)
+    
+plt.figure(1)
+plt.subplot(121)
+plt.imshow(canvas)
+plt.subplot(122)
+plt.imshow(img)
+plt.show()
+
+plt.plot(range(n_shapes), losses, color="green")
+plt.show()
--- src/shape.py
+++ src/shape.py
@@ -1,22 +1,18 @@
 import numpy as np
 import cv2
+import random
 
 class Shape:
     def __init__(self, constraints):
         self.epsilon = 0
-        self.colour = np.random.choice(constraints['all_colours'])
+        self.colour = random.choice(constraints['all_colours'])
         self.x1 = np.random.randint(0, constraints['width'])
         self.y1 = np.random.randint(0, constraints['height'])
         self.x2 = np.random.randint(0, constraints['width'])
         self.y2 = np.random.randint(0, constraints['height'])
         self.rotation = np.random.choice([0, 45, 90])
         
-    def superimpose(self, img):
-        new_img = cv2.rectangle(img, (self.x1, self.y1), (self.x2, self.y2), self.colour, -1)
+    def superimpose(self, og_img):
+        new_img = cv2.rectangle(og_img, (self.x1, self.y1), (self.x2, self.y2), self.colour, -1)
         
-        return new_img, img
-        
-    def get_score(self, og, gen, gen_with_shape):
-        
-        
-    
+        return new_img
